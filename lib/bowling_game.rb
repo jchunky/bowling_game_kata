1f37bf75da358cbf33d91fd3cd66be7111feaca3
Chunky <jasonc@nulogy.com>
2019-12-11 18:01:03 -0500

    Refactor

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -3,7 +3,6 @@ require 'ostruct'
 class BowlingGame
   MAX_POINTS_PER_TURN = 10
   MAX_TURNS = 10
-  attr_reader :roll_number, :turn, :turn_scores, :spare
 
   def initialize
     @rolls = []
@@ -11,7 +10,6 @@ class BowlingGame
 
   def roll(points)
     @rolls << points
-    score
   end
 
   def score
@@ -22,57 +20,49 @@ class BowlingGame
     @spare = false
     @strikes = []
     @bonus_rolls = 0
-    @rolls.each { |roll| score_roll(roll) }
-    @score
-  end
+    @rolls.each do |points|
+      break if @turn == MAX_TURNS
 
-  private
-
-  def score_roll(points)
-    return 'game over' if @turn == MAX_TURNS
-
-    @score += points
-    @turn_scores[@turn] += points
-    add_spare_points(points) if @spare
-
-    @strikes.each do |strike|
-      if !strike.counted
+      @score += points
+      @turn_scores[@turn] += points
+      if @spare
         @score += points
-        @turn_scores[strike.turn] += points
-        strike.count += 1
+        @turn_scores[(@turn - 1)] += points
+        @spare = false
       end
 
-      !strike.counted = true if strike.count == 2
-    end unless @strikes.empty?
+      @strikes.each do |strike|
+        if !strike.counted
+          @score += points
+          @turn_scores[strike.turn] += points
+          strike.count += 1
+        end
 
-    if @bonus_rolls.zero?
-      if @turn < 9
-        @strikes << OpenStruct.new(turn: @turn, count: 0, counted: false) if @roll_number.zero? && points == MAX_POINTS_PER_TURN
-        @spare = true if @roll_number == 1 && @turn_scores[@turn] == MAX_POINTS_PER_TURN
-        increment_roll_number(points)
-        @turn += 1 if @roll_number.zero?
-      elsif points == MAX_POINTS_PER_TURN && @roll_number.zero?
-        @bonus_rolls = 2
-      elsif @turn_scores[@turn] == MAX_POINTS_PER_TURN && roll_number == 1
-        @bonus_rolls = 1
+        !strike.counted = true if strike.count == 2
+      end unless @strikes.empty?
+
+      if @bonus_rolls.zero?
+        if @turn < 9
+          @strikes << OpenStruct.new(turn: @turn, count: 0, counted: false) if @roll_number.zero? && points == MAX_POINTS_PER_TURN
+          @spare = true if @roll_number == 1 && @turn_scores[@turn] == MAX_POINTS_PER_TURN
+          if points == MAX_POINTS_PER_TURN
+          else
+            @roll_number = @roll_number.zero? ? 1 : 0
+          end
+          @turn += 1 if @roll_number.zero?
+        elsif points == MAX_POINTS_PER_TURN && @roll_number.zero?
+          @bonus_rolls = 2
+        elsif @turn_scores[@turn] == MAX_POINTS_PER_TURN && roll_number == 1
+          @bonus_rolls = 1
+        else
+          @turn += 1
+        end
       else
-        @turn += 1
+        @bonus_rolls -= 1
+        @turn += 1 if @bonus_rolls.zero?
       end
-    else
-      @bonus_rolls -= 1
-      @turn += 1 if @bonus_rolls.zero?
     end
-  end
 
-  def add_spare_points(points)
-    @score += points
-    @turn_scores[(@turn - 1)] += points
-    @spare = false
-  end
-
-  def increment_roll_number(points)
-    return if points == MAX_POINTS_PER_TURN
-
-    @roll_number = @roll_number.zero? ? 1 : 0
+    @score
   end
 end
