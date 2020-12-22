6bb471524ab95f40c0c6dcddbc73dd591dc69185
Chunky <jasonc@nulogy.com>
2020-12-21 23:23:12 -0500

    refactor

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,79 +1,37 @@
-require 'ostruct'
-
 class BowlingGame
-  MAX_POINTS_PER_TURN = 10
-  MAX_TURNS = 10
+  attr_reader :rolls
 
   def initialize
     @rolls = []
   end
 
   def roll(points)
-    @rolls << points
+    rolls << points
   end
 
   def score
-    @score = 0
-    @roll_number = 0
-    @turn = 0
-    @turn_scores = Array.new(MAX_TURNS, 0)
-    @spare = false
-    @strikes = []
-    @bonus_rolls = 0
+    rolls.zip(factors).sum { |r, f| r * f }
+  end
 
-    @rolls.each do |points|
-      if @turn == MAX_TURNS
-        break
-      end
+  def factors
+    result = [0] * 21
+    frames = [[]]
+    rolls.each_with_index do |roll, i|
+      break if frames.size > 10
 
-      @score += points
-      @turn_scores[@turn] += points
-      if @spare
-        @score += points
-        @turn_scores[(@turn - 1)] += points
-        @spare = false
-      end
-
-      @strikes.each do |strike|
-        if !strike.counted
-          @score += points
-          @turn_scores[strike.turn] += points
-          strike.count += 1
-        end
-        if strike.count == 2
-          strike.counted = true
-        end
-      end
-
-      if @bonus_rolls.zero?
-        if @turn < 9
-          if @roll_number.zero? && points == MAX_POINTS_PER_TURN
-            @strikes << OpenStruct.new(turn: @turn, count: 0, counted: false)
-          end
-          if @roll_number == 1 && @turn_scores[@turn] == MAX_POINTS_PER_TURN
-            @spare = true
-          end
-          if points != MAX_POINTS_PER_TURN
-            @roll_number = @roll_number.zero? ? 1 : 0
-          end
-          if @roll_number.zero?
-            @turn += 1
-          end
-        elsif points == MAX_POINTS_PER_TURN && @roll_number.zero?
-          @bonus_rolls = 2
-        elsif @turn_scores[@turn] == MAX_POINTS_PER_TURN && roll_number == 1
-          @bonus_rolls = 1
-        else
-          @turn += 1
-        end
-      else
-        @bonus_rolls -= 1
-        if @bonus_rolls.zero?
-          @turn += 1
-        end
+      frames.last << roll
+      result[i] += 1
+      if frames.last.sum == 10 && frames.last.size == 1
+        result[i + 1] += 1
+        result[i + 2] += 1
+        frames << []
+      elsif frames.last.sum == 10
+        result[i + 1] += 1
+        frames << []
+      elsif frames.last.size == 2
+        frames << []
       end
     end
-
-    @score
+    result
   end
 end
